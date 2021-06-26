9b4f4e71cfca4349d2b33829c87c8f8d37f28d88
Michael S <43636601+MichaelCoding25@users.noreply.github.com>
2021-06-26 23:07:44 +0300

    Fix more typos

--- persistent-flashlight/config.lua
+++ persistent-flashlight/config.lua
@@ -1,10 +1,10 @@
 Config = {
-	always_on = false, -- If true then weaponlights and flashlight are always persistent and cannot be modifed by individial players, but if false then players can use the command to turn the persistence on and off.
+	always_on = false, -- If true then weaponlights and flashlight are always persistent and cannot be modified by individual players, but if false then players can use the command to turn the persistence on and off.
 
 	command = 'plight', -- The command name to be used if always_on is set to true.
 	description = 'Toggle flashlight being on even while not aiming it', -- The description of the command.
 
 	notifications = true, -- Set to true if you want notifications when using the command, set to false if you don't.
-	notif_on = '~g~You turned persistent flashlight on', -- Notification for when turning persistance on.
-	notif_off = '~r~You turned persistent flashlight off', -- Notification for when turning persistance off.
+	notif_on = '~g~You turned persistent flashlight on', -- Notification for when turning persistence on.
+	notif_off = '~r~You turned persistent flashlight off', -- Notification for when turning persistence off.
 }
